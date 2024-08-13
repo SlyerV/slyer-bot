4caa4bbacbed96f4951a5ee2af97fd7822864747
SlyerV <80004341+SlyerV@users.noreply.github.com>
2024-08-13 16:57:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1421,7 +1421,7 @@ client.on("interactionCreate", async int => {
 				    r+=" "
 			    } else if (id === "~") {
 				    if (! flipped) {
-					    int.editReply({components:[frow2,frow3,frow4]})
+					    int.editReply({components:[frow1,frow2,frow3,frow4,frow5]})
 					    flipped = true
 				    } else {
 					    int.editReply({components:[row1,row2,row3,row4,row5]})
