adcce928f9e50b18e13fd0731ef092c8c6793aaa
SlyerV <80004341+SlyerV@users.noreply.github.com>
2024-08-17 21:24:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -909,13 +909,13 @@ client.on("interactionCreate", async int => {
 					    int.reply("**"+root+"** is the answer to "+a+"x^2 + "+b+"x + "+c)
 				    } else if (! Number.isInteger(Math.sqrt(discrim))){
 					    const space = " "
-					    const numerator1 = "__"+(-1*b)+"+ √"+discrim+"__"
-					    const root1 = numerator1+"\n"+(space.repeat(numerator1.length/2))+(2*a)+(space.repeat(numerator1.length/2))+",\n"
-					    const numerator2 = "__"+(-1*b)+"- √"+discrim+"__"
-					    const root2 = numerator2+"\n"+(space.repeat(numerator2.length/2))+(2*a)+(space.repeat(numerator2.length/2))+","
+					    const lineone1 = "__"+(-1*b)+"+ √"+discrim+"__"
+					    const linetwo1 = (space.repeat(numerator1.length/2))+(2*a)+(space.repeat(numerator1.length/2))+",    "
+					    const lineone2 = "    __"+(-1*b)+"- √"+discrim+"__"
+					    const linetwo2 = (space.repeat(numerator2.length/2))+(2*a)+(space.repeat(numerator2.length/2))
 					    console.log(root1)
 					    console.log(root2)
-					    int.reply(root1+" & "+root2+" are the answers")
+					    int.reply(lineone1+lineone2+"\n"+linetwo1+linetwo2+" are the answers")
 				    } else {
 					    const root1 = ((-1*b)+Math.sqrt(discrim))/(2*a)
 					    const root2 = ((-1*b)-Math.sqrt(discrim))/(2*a)
