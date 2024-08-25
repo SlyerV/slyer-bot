aec8e9125921fc39a2ad4c21d6ab4700daab42e2
SlyerV <80004341+SlyerV@users.noreply.github.com>
2024-08-25 14:11:43 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2050,35 +2050,35 @@ client.on("messageCreate", async msg => {
 		    if (ni == 1) {
 				resp = await msg.channel.send({content:"A random loot box appeared!!! First one to click the accept button gets **A ONE-TIME-USE N WORD PASS**!!!\n# <a:loot:1271692935389380608>", components: [row]})
 		    } else {
-				resp = await msg.channel.send({content:"A random loot box appeared!!! First one to click the accept button gets 25 XP!!!\n# <a:loot:1271692935389380608>", components: [row]})
+				resp = await msg.channel.send({content:"A random loot box appeared!!! First one to click the accept button gets 20 XP!!!\n# <a:loot:1271692935389380608>", components: [row]})
 		    }
 		    try {
 		    	confirmation = await resp.awaitMessageComponent({ time: 5_000 })
 			if (confirmation.customId === "loot+") {
 				if (ni != 1) {
-					resp.reply(`<@${confirmation.user.id}> GOT THE 25 XP!!!\n# +25 XP`)
+					resp.reply(`<@${confirmation.user.id}> GOT THE 20 XP!!!\n# +20 XP`)
 				} else {
 					resp.reply(`<@${confirmation.user.id}> GOT THE N-WORD PASS!!!\n You will not be warned for saying the n-word. One time use only. Terms and conditions apply.`)
 				}
 				const oldrank = level(xp[confirmation.user.id])
-				xp[confirmation.user.id] = xp[confirmation.user.id] + 25
+				xp[confirmation.user.id] = xp[confirmation.user.id] + 20
 				writexp()
 				const newrank = level(xp[confirmation.user.id])
 			        if (newrank > oldrank) {
 				    msg.channel.send("# <@"+confirmation.user.id+"> LEVEL UP! "+oldrank+" → "+newrank)
 			        }
-				resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 25 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
+				resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 20 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
 			} else if (confirmation.customId === "loot-") {
 				if (ni != 1) {
-					resp.reply("Soooo... <@"+msg.author.id+"> you just denied an opportunity to gain 25 XP huh; to anyone who wanted the XP sorry ig :(")
+					resp.reply("Soooo... <@"+msg.author.id+"> you just denied an opportunity to gain 20 XP huh; to anyone who wanted the XP sorry ig :(")
 				} else {
 					resp.reply("Soooo... <@"+msg.author.id+"> you just deleted the N-Word pass, nice! You def made some ppl very happy but for others im not so sure...")
 				}
-				resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 25 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
+				resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 20 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
 			}
 		    } catch {
 			    resp.reply("Damn y'all were too late...")
-			    resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 25 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
+			    resp.edit({content:"A random loot box appeared!!! First one to click the accept button gets 20 XP!!!\n# <a:loot:1271692935389380608>",components:[]})
 		    }
 		    
 	    }
