ca5a3a8012422c51c2d3486f0669b2d44835228c
SlyerV <80004341+SlyerV@users.noreply.github.com>
2024-09-13 22:29:09 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1213,7 +1213,11 @@ client.on("interactionCreate", async int => {
 				    await int.reply("You're Level 10, so you don't need any more XP to level up! You have **"+xp[int.user.id]+"** XP.")
 			    }
 		    } else if (subint === "getfile") {
-			    int.reply(xp)
+			    fs.readFile('xp.json', (err, data) => {
+				  if (err) { throw err; }
+				  const xp = JSON.stringify(JSON.parse(data), null, 2);
+				  await int.reply('```json\n' + xp + '\n```');
+			    });
 		    }
 	     } else if (int.commandName === "help") {
 		    let str
